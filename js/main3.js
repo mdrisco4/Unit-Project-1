f6413e63e30b171acbf3a30b69bbfa16d238c317
michaeldriscoll <michaelndriscoll81@gmail.com>
2019-12-06 13:25:25 -0500

    looks nice, can fix scoring bugs in free time

--- js/main3.js
+++ js/main3.js
@@ -801,7 +801,6 @@ function bonusMovieQuestion () {
         },{once : true})
     }
     roundCounter++
-    console.log(roundCounter)
 }
 
 function bonusTVQuestion () {
@@ -852,8 +851,6 @@ function bonusBooksQuestion () {
         },{once : true})
     }
     roundCounter++
-    console.log(score)
-    console.log(1/roundCounter)
 }
 
 function renderMovieQuestion () {
@@ -876,13 +873,13 @@ function renderMovieQuestion () {
                 score += 1/roundCounter
                 getH2.innerHTML = movies[roundCounter - 1].correctAnswer;
                 document.querySelector(".right-wrong").innerHTML = "&#128512 Correct! &#128512"
-                document.querySelector(".score").innerHTML = "Score:" + " " + Math.ceil(score)
+                document.querySelector(".score").innerHTML = "&#11088 Score:" + " " + Math.ceil(score) + " " + " &#11088"
             } else {
                 getH2.innerHTML = movies[roundCounter - 1].correctAnswer;
                 document.querySelector(".right-wrong").innerHTML = "&#128577 No Luck &#128577"
-                document.querySelector(".score").innerHTML = "Score:" + " " + Math.ceil(score)
+                document.querySelector(".score").innerHTML = "&#11088 Score:" + " " + Math.ceil(score) + " " + " &#11088"
             }
-        },{once : true})
+         },{once : true})
     }
     roundCounter++
 }
@@ -908,11 +905,11 @@ function renderTVQuestion () {
                 score += 1/roundCounter
                 getH2.innerHTML = tv[roundCounter - 1].correctAnswer;
                 document.querySelector(".right-wrong").innerHTML = "&#128512 Correct! &#128512"
-                document.querySelector(".score").innerHTML = "Score:" + " " + Math.ceil(score)
+                document.querySelector(".score").innerHTML = "&#11088 Score:" + " " + Math.ceil(score) + " " + " &#11088"
             } else {
                 getH2.innerHTML = tv[roundCounter - 1].correctAnswer;
                 document.querySelector(".right-wrong").innerHTML = "&#128577 No Luck &#128577"
-                document.querySelector(".score").innerHTML = "Score:" + " " + Math.ceil(score)
+                document.querySelector(".score").innerHTML = "&#11088 Score:" + " " + Math.ceil(score) + " " + " &#11088"
             }
         },{once : true})
     }
@@ -939,11 +936,11 @@ function renderBooksQuestion () {
                 score += 1/roundCounter
                 getH2.innerHTML = books[roundCounter - 1].correctAnswer;
                 document.querySelector(".right-wrong").innerHTML = "&#128512 Correct! &#128512"
-                document.querySelector(".score").innerHTML = "Score:" + " " + Math.ceil(score)
+                document.querySelector(".score").innerHTML = "&#11088 Score:" + " " + Math.ceil(score) + " " + " &#11088"
             } else {
                 getH2.innerHTML = books[roundCounter - 1].correctAnswer;
                 document.querySelector(".right-wrong").innerHTML = "&#128577 No Luck &#128577"
-                document.querySelector(".score").innerHTML = "Score:" + " " + Math.ceil(score)
+                document.querySelector(".score").innerHTML = " &#11088 Score:" + " " + Math.ceil(score) + " " + " &#11088"
             }
         },{once : true})
     }
@@ -991,125 +988,4 @@ document.querySelector(".books").addEventListener('click', function() {
 })
 
 
-///////POTENTIAL BONUS QUESTIONS///////
-
-// MOVIES
-
-/*{
-        question: "Bruce Willis played a time travelling criminal in what movie?",
-        choices: [
-            {
-                text: "Hudson Hawk",
-                image: "images/HudsonHawk.jpeg",
-                isCorrect: false
-            },
-            {
-                text: "Die Hard With A Vengence",
-                image: "images/dieHard3.jpeg",
-                isCorrect: false
-            },
-            {
-                text: "Twelve Monkeys",
-                image: "images/12Monkeys.jpeg",
-                isCorrect: true
-            },
-            {
-                text: "THe Fifth Element",
-                image: "images/fifthElement.jpeg",
-                isCorrect: false
-            },
-            {
-                text: "Red",
-                image: "images/red.jpeg",
-                isCorrect: false
-            },
-            {
-                text: "The Whole Nine Yards",
-                image: "images/wholeNine.jpeg",
-                isCorrect: false
-            }
-        ],
-        correctAnswer: "Answer: Twelve Monkeys"
-    },
-    */
-
-// TV
-/*{
-        question: "Where was the American crime drama `Narcos` filmed?",
-        choices: [
-            {
-                text: "Nicaragua",
-                image: "images/nicaragua.jpeg",
-                isCorrect: false
-            },
-            {
-                text: "Venezuela",
-                image: "images/venezuela.jpeg",
-                isCorrect: false
-            },
-            {
-                text: "Bolivia",
-                image: "images/bolivia.jpeg",
-                isCorrect: false
-            },
-            {
-                text: "Bolize",
-                image: "images/bolize.jpeg",
-                isCorrect: false
-            },
-            {
-                text: "Columbia",
-                image: "images/columbia.jpeg",
-                isCorrect: true
-            },
-            {
-                text: "The Moon",
-                image: "images/moon.jpeg",
-                isCorrect: false
-            }
-        ],
-        correctAnswer: "Answer: Columbia"
-    },
-/*
-
-// BOOKS
-/*{
-        question: "What was the working title of the 1936 novel Gone With the Wind?",
-        choices: [
-            {
-                text: "The Plantation",
-                image: "images/goneWithTheWind1.jpeg",
-                isCorrect: false
-            },
-            {
-                text: "Ba! Ba! Black Sheep",
-                image: "images/goneWithTheWind2.jpeg",
-                isCorrect: true
-            },
-            {
-                text: "The Rhett Butler Affair",
-                image: "images/goneWithTheWind3.jpeg",
-                isCorrect: false
-            },
-            {
-                text: "The Art of the Deal",
-                image: "images/goneWithTheWind4.jpeg",
-                isCorrect: false
-            },
-            {
-                text: "It",
-                image: "images/goneWithTheWind5.jpeg",
-                isCorrect: false
-            },
-            {
-                text: "Tara",
-                image: "images/goneWithTheWind6.jpeg",
-                isCorrect: false
-            }
-        ]
-        ,
-        correctAnswer: "Ba! Ba! Black Sheep"
-    },
-    */
-  
-    
+///////POTENTIAL BONUS QUESTIONS IN mian.js///////
